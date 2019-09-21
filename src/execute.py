19a502a599508609ca48299c5314dfa331edc40a
Gu-youngfeng <yongfeng_gu@163.com>
2019-09-21 14:48:24 +0800

    Update execute.py
    The execute.py is the running script of step 1 to step 4.

--- src/execute.py
+++ src/execute.py
@@ -1,9 +1,26 @@
 #!/usr/bin/python3
 # coding=utf-8
 
+"""
+@description:
+running script of step-1 to step-4.
+
+@author  : Yongfeng
+@reviewer: Yongfeng
+"""
+
 import os
 
 if __name__ == "__main__":
+
+	print("Happy Starting ~")
+	# step-1: prepare the datasets in "raw-data/"
+
+	# step-2: obtain the ranking list using the rank-based approach
+	rank_cmd = "python rank_based.py"
+	os.system(rank_cmd)
+
+	# step-3: obtain the ranking lists using different approaches
 	methods = ["classification.py", "outlier_detection.py", "random_rank.py", "reconfig.py", "direct_ltr.py"]
 	for method in methods:
 		if not os.path.exists(method):
@@ -11,3 +28,10 @@ if __name__ == "__main__":
 			continue
 		exec_cmd = "python " + method
 		os.system(exec_cmd)
+
+	# step-4: calculate the RDTie of ranking lists
+	cal_rdtie_cmd = "python experiment.py calRDTie"
+	os.system(cal_rdtie_cmd)
+	
+	print("Happy ending ~")
+	
